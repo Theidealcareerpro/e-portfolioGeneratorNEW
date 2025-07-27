e647a44986f593d2730a1b44ea0ece393b501812
Theidealcareerpro <theidealcareerpro@gmail.com>
2025-07-27 11:13:30 +0100

    Update submit-feedback.js

--- functions/submit-feedback.js
+++ functions/submit-feedback.js
@@ -1,9 +1,14 @@
+const querystring = require('querystring');
+
 exports.handler = async (event) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
   try {
-    const data = JSON.parse(event.body);
+    const data = querystring.parse(event.body);
+    if (!data.message) {
+      throw new Error('No message provided in form data');
+    }
     console.log('Feedback received:', data.message);
     return {
       statusCode: 302,
